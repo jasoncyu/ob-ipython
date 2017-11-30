4d95eb37c50d40c3803a3e075a881197d5e27cde
jasoncyu <yujason2@gmail.com>
2017-11-30 12:47:23 -0800

    Add split block from scimax

--- ob-ipython.el
+++ ob-ipython.el
@@ -676,6 +676,25 @@ Make sure your src block has a :session param.")
            replacement
            (cdr (assoc :result-params (nth 2 (org-babel-get-src-block-info)))))
           (org-redisplay-inline-images))))))
+;; Take some stuff from scimax
+
+(defun org-babel-split-src-block (&optional below)
+  "Split the current src block.
+With a prefix BELOW move point to lower block."
+  (interactive "P")
+  (let* ((el (org-element-context))
+         (language (org-element-property :language el))
+         (parameters (org-element-property :parameters el)))
+
+    (beginning-of-line)
+    (insert (format "#+END_SRC
+
+#+BEGIN_SRC %s %s\n" language (or parameters "")))
+    (beginning-of-line)
+    (when (not below)
+      (org-babel-previous-src-block))))
+
+;; (define-key org-mode-map (kbd "H--") #'org-babel-split-src-block)
 
 ;; lib
 
